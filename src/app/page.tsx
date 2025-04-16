ca2b2429147ca6b31555d4465998baf030b137dc
m-ghiasi <mahdiye71gh@gmail.com>
2025-04-16 12:50:56 +0430

    first page effects committed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+
+"use client"
 import Image from "next/image";
 import Logo from "../../public/Logo.svg";
 import Elipse from "../../public/Ellipse 1006.svg";
@@ -21,18 +23,18 @@ export default function Home() {
   return (
     <div className="relative bg-gray-200 w-full min-h-screen rounded-2xl  max-w-[390px]">
       <Image
-        className={`${isActive ? "translate-x-0": "translate-x-[200px]"} w-52 h-52 absolutet top-0 left-0 transform transition-transform duration-200 `}
+        className={`${isActive ?"translate-x-0": "translate-x-[-250px]"} w-52 h-52 absolute top-[-50px] left-[-25px] transform transition-transform duration-300 `}
         
         src={Elipseg}
         alt="design first page of maya food"
       />
       
-      <div className="flex justify-center items-center ">
+      <div className="absolute top-1/2 left-1/2  transform -translate-x-1/2 -translate-y-1/">
         <Image alt="logo from maya food order application" src={Logo} />
       </div>
 
       <Image
-        className=" w-64 h-64 absolute  bottom-0 right-0"
+      className={`${isActive ?"translate-x-0 ": "  translate-x-[-650px]"} w-64 h-64 absolute  bottom-0 right-0 transform transition-transform duration-300 `}
         src={Elipse}
         alt="desige first page of mayafood app"
       />
