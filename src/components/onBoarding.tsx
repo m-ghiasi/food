35fe4b8980ab362667f10207c0e26aea7751f304
m-ghiasi <mahdiye71gh@gmail.com>
2025-04-19 12:20:42 +0430

    st font "Sen"

--- src/components/onBoarding.tsx
+++ src/components/onBoarding.tsx
@@ -22,15 +22,15 @@ return(
     <div className=" h-screen flex flex-col justify-center items-center p-5">
 
         {/* content */}
-        <div className="bg-gray-500 rounded-2xl w-60 h-72 p-5">
+        <div className="bg-gray-400 rounded-2xl w-60 h-72 p-5">
             
 
         </div>
 
         {/* discriptions */}
         <div className="mt-8 flex flex-col items-center">
-        <h1 className="text-2xl font-bold">All your favorites</h1>
-        <p className="text-gray-700 pt-4 text-center ">Get all your loved foods in one once place,
+        <h1 className="text-2xl font-[800]">All your favorites</h1>
+        <p className="text-gray-600 pt-4 text-center ">Get all your loved foods in one once place,
         you just place the orer we do the rest</p>
         </div>
 
@@ -50,11 +50,11 @@ return(
 
 
         {/* buttons */}
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 mt-14">
             <Button 
             onClick={goNext}
             label="NEXT" className={clsx(
-                'text-white bg-[#FF7622] w-[327px] py-5 rounded-2xl'
+                'text-white bg-[#FF7622] w-[327px] py-5 rounded-2xl '
             )}/>
             <Button label="Skip" className={clsx(
                 'text-gray-600 bg-white'
