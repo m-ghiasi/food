19552efbe41b63dec44e9e2251e6b661bef33a22
m.ghiasi <mahdiye71gh@gmail.com>
2025-10-09 18:05:08 -0700

    tailwind config

---
+++ src/lib/utils/index.ts
@@ -0,0 +1,7 @@
+// app/utils/cn.ts
+import { twMerge } from "tailwind-merge";
+import clsx, { ClassValue } from "clsx";
+
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(...inputs));
+}
